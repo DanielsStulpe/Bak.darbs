cd4e828d342885d1e8c6df837edb52a768ed8b3f
Daniels Stulpe <Daniels.Stulpe@edu.rtu.lv>
2026-02-12 10:41:52 +0200

    Yolo12n

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo12m.pt")
+model = YOLO("yolo12n.pt")
 
 model.train(
     data="roboflow_dataset/data.yaml",
     epochs=100,
     imgsz=640,
-    batch=4,
     workers=1,
     device=0  # GPU (Jetson)
 )
