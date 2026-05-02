02d9403df983f2c74841ce41274289437e198cf4
Daniels Stulpe <Daniels.Stulpe@edu.rtu.lv>
2026-05-02 10:11:34 +0300

    Update script

--- batch_size_exp.py
+++ batch_size_exp.py
@@ -88,7 +88,7 @@ for batch_size in BATCH_SIZES:
             lr0=0.001,
             workers=4,
             seed=0,
-            device=device,
+            device=0 if batch_size == 1 else device,
             name=f"yolo26_bs{batch_size}"
         )
 
